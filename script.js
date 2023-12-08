9278797a697360212b375b2e527e13fb3f867fee
Priyambada216 <144552804+Priyambada216@users.noreply.github.com>
2023-12-08 19:08:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 function functionName() {
-	const name = Function.prototype.toString.call(this).match(/\s*function\s+([^\s(]+)/)[1];
-  alert(name);
+  alert(arguments.callee.name);
 }
+
 functionName();
 
